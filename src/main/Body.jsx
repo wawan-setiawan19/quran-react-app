729662a425189bed34f7a9014a060c77997352fe
onepro19 <w.settiawan@gmail.com>
2022-07-12 18:44:51 +0700

    add link

--- src/main/Body.jsx
+++ src/main/Body.jsx
@@ -2,7 +2,8 @@ const Body = () =>{
     return (
         <div>
             Ini mau coba link buat diklik
-            <a href="http://siasiqu.smpiqualbahjah1.sch.id">SIASIQU</a>
+            <br /><a href="http://siasiqu.smpiqualbahjah1.sch.id">SIASIQU</a>
+            <br /><a href="http://eraport.smpiqualbahjah1.sch.id">E RAPORT</a>
         </div>
     )
 }
